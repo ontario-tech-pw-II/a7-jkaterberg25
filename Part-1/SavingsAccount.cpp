f7fc77f5b34d76a789cab9967df64cdab2ead1f2
Justin Katerberg <j.katerberg10@gmail.com>
2020-02-27 12:33:47 -0500

    Part-1

--- Part-1/SavingsAccount.cpp
+++ Part-1/SavingsAccount.cpp
@@ -5,13 +5,20 @@ using namespace std;
 // constructor initializes balance and interest rate
 SavingsAccount::SavingsAccount( double initialBalance, double rate ) : Account( initialBalance ) 
 {
-	// your code
+	if(rate >= 0){
+		this->rate = rate;
+	}else{
+		this->rate = 0;
+	}
 }
 
 double SavingsAccount::calculateInterest() {
-	// your code
+	return (getBalance() * rate);
 } 
+
 void SavingsAccount::display(ostream & os) const
 {
-	// your code
+	cout << "Account Type: Saving" << endl
+		<< "Balance($): " << getBalance() << endl
+		<< "Rate: " << rate << endl;
 }
