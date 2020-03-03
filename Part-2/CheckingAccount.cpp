66535b4245d5f114ff227fc8eb3efbeab3b1c1d5
Justin Katerberg <j.katerberg10@gmail.com>
2020-03-03 11:41:15 -0500

    Part-2

--- Part-2/CheckingAccount.cpp
+++ Part-2/CheckingAccount.cpp
@@ -6,28 +6,42 @@ using namespace std;
 // constructor initializes balance and transaction fee
 CheckingAccount::CheckingAccount( double initialBalance, double fee ) : Account( initialBalance ) 
 {
-	// your code
+	transactionFee = fee;
 } 
 
 // credit (add) an amount to the account balance and charge fee
 void CheckingAccount::credit( double amount )
 {
-	// your code
+	setBalance(getBalance()+amount);
+	chargeFee();
 } 
 
 // debit (subtract) an amount from the account balance and charge fee
 bool CheckingAccount::debit( double amount )
 {
-	// your code
+	if(getBalance()-amount-transactionFee > 0){
+		setBalance(getBalance()-amount);
+		chargeFee();
+		return 1;
+	}else{
+		return 0;
+	}
+
 }
 
 // subtract transaction fee
 void CheckingAccount::chargeFee()
 {
-	// your code
+	setBalance(getBalance() - transactionFee);
 }
 
 void CheckingAccount::display(ostream & os) const
 {
-	// your code
+	os.precision(2);
+	os.precision(2);
+	os << fixed;
+	os << fixed;
+	os << "Account Type: Checking" << endl
+		<< "Balance: $" << getBalance() << endl
+		<< "Transaction Fee: $" << transactionFee << endl;
 }
--- Part-2/SavingsAccount.cpp
+++ Part-2/SavingsAccount.cpp
@@ -5,13 +5,22 @@ using namespace std;
 // constructor initializes balance and interest rate
 SavingsAccount::SavingsAccount( double initialBalance, double rate ) : Account( initialBalance ) 
 {
-	// your code
+	if(rate >= 0){
+		interestRate = rate;
+	}else{
+		interestRate = 0;
+	}
 }
 
 double SavingsAccount::calculateInterest() {
-	// your code
+	return (getBalance() * interestRate);
 } 
+
 void SavingsAccount::display(ostream & os) const
 {
-	// your code
+	os.precision(2);
+	os << fixed;
+	os << "Account Type: Saving" << endl
+		<< "Balance($): " << getBalance() << endl
+		<< "Rate: " << interestRate*100 << "%" <<  endl;
 }
